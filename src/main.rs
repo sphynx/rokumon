dec2e06bf07687fdba9196380f65ecdde043a434
Ivan Veselov <veselov@gmail.com>
2020-04-12 17:44:38 +0100

    Implement a parser for moves

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use std::convert::TryFrom;
 use std::fmt;
 use std::str::FromStr;
 
@@ -112,6 +113,11 @@ impl fmt::Display for Coord {
 }
 
 impl Coord {
+    #[allow(dead_code)]
+    fn new(x: i8, y: i8, z: i8) -> Self {
+        Coord { x, y, z }
+    }
+
     /// Convert between two types of coordinates in presence of given
     /// board.
     #[allow(dead_code)]
@@ -142,7 +148,7 @@ impl Coord {
 /// More user-friendly coordinates easier to undertand and type. Row
 /// is counted from 1 and start from the top. Card is counted from 1
 /// and starts from the left.
-#[derive(PartialEq, Eq, Debug)]
+#[derive(PartialEq, Eq, Debug, Clone)]
 struct UserCoord {
     row: u8,
     card: u8,
@@ -166,6 +172,13 @@ impl FromStr for UserCoord {
                 _ => bail!(usage),
             };
         }
+    }
+}
+
+#[allow(dead_code)]
+impl UserCoord {
+    fn new(row: u8, card: u8) -> Self {
+        UserCoord { row, card }
     }
 }
 
@@ -469,7 +482,7 @@ impl Game {
         }
     }
 
-    fn is_valid_move(&self, game_move: &GameMove) -> bool {
+    fn is_valid_move(&self, game_move: &GameMove<Coord>) -> bool {
         use GameMove::*;
         match game_move {
             Place(die, coord) => {
@@ -520,7 +533,7 @@ impl Game {
     }
 
     #[allow(dead_code)]
-    fn apply_move(&mut self, game_move: &GameMove) -> Fallible<()> {
+    fn apply_move(&mut self, game_move: &GameMove<Coord>) -> Fallible<()> {
         use GameMove::*;
 
         if self.is_valid_move(game_move) {
@@ -640,12 +653,12 @@ impl Game {
     }
 }
 
-#[allow(dead_code)]
-enum GameMove {
-    Place(Die, Coord),
-    Move(Die, Coord, Coord),
-    Fight(Coord),
-    Surprise(Coord, Coord),
+#[derive(PartialEq, Eq, Debug, Clone)]
+enum GameMove<C> {
+    Place(Die, C),
+    Move(Die, C, C),
+    Fight(C),
+    Surprise(C, Coord),
     Submit,
 }
 
@@ -656,45 +669,213 @@ fn main() {
     println!("{}", game);
 }
 
-
+/// We are parsing the following format, informally given by example:
+/// - place W1 at R2C2
+/// - move B3 from R1C1 to R1C2
+/// - fight at R2C3
+/// - surprise from R1C2 to <3, -2, -1>
+/// - submit
+#[allow(dead_code)]
 mod parsers {
-    use nom::IResult;
+    use super::*;
 
-    //use nom::bytes::complete::tag;
-    //use nom::character::complete::alpha1;
-    use nom::character::complete::char;
-    use nom::character::complete::digit1;
-    //use nom::character::complete::line_ending;
-    use nom::combinator::map;
-    use nom::combinator::map_res;
-    use nom::combinator::value;
-    //use nom::multi::separated_nonempty_list;
-    use nom::sequence::pair;
     use nom::branch::alt;
+    use nom::bytes::complete::tag_no_case;
+    use nom::character::complete::{char, digit1, one_of, space0, space1};
+    use nom::combinator::{map, map_opt, map_res, value};
+    use nom::sequence::{delimited, pair, terminated, tuple};
+    use nom::IResult;
+    use nom::{do_parse, tag_no_case};
 
-    #[allow(dead_code)]
     fn unsigned(i: &str) -> IResult<&str, i8> {
         map_res(digit1, |s: &str| s.parse())(i)
     }
 
-    #[allow(dead_code)]
+    fn u8_parser(i: &str) -> IResult<&str, u8> {
+        map_res(digit1, |s: &str| s.parse())(i)
+    }
+
     fn sign(i: &str) -> IResult<&str, i8> {
         alt((value(-1, char('-')), |i| Ok((i, 1))))(i)
     }
 
-    #[allow(dead_code)]
     fn signed(i: &str) -> IResult<&str, i8> {
         map(pair(sign, unsigned), |(s, u)| s * u)(i)
     }
 
-    #[cfg(test)]
-    mod test {
-        #[test]
-        fn test1() {
-            assert_eq!(1+1, 2);
-        }
+    fn die_value(i: &str) -> IResult<&str, u8> {
+        map_opt(one_of("123456"), |c| c.to_digit(10).and_then(|d| u8::try_from(d).ok()))(i)
     }
 
+    fn die_color(i: &str) -> IResult<&str, DiceColor> {
+        alt((
+            value(DiceColor::Black, tag_no_case("B")),
+            value(DiceColor::White, tag_no_case("W")),
+            value(DiceColor::Red, tag_no_case("R")),
+        ))(i)
+    }
+
+    fn die(i: &str) -> IResult<&str, Die> {
+        map(pair(die_color, die_value), |(color, value)| Die { color, value })(i)
+    }
+
+    fn user_coord(i: &str) -> IResult<&str, UserCoord> {
+        do_parse!(
+            i,
+            tag_no_case!("R") >> row: u8_parser >> tag_no_case!("C") >> card: u8_parser >> (UserCoord { row, card })
+        )
+    }
+
+    fn coord_sep(i: &str) -> IResult<&str, ()> {
+        value((), terminated(char(','), space0))(i)
+    }
+
+    fn coord(i: &str) -> IResult<&str, Coord> {
+        map(
+            delimited(
+                char('<'),
+                tuple((signed, coord_sep, signed, coord_sep, signed)),
+                char('>'),
+            ),
+            |(x, _, y, _, z)| Coord { x, y, z },
+        )(i)
+    }
+
+    fn place_cmd(i: &str) -> IResult<&str, GameMove<UserCoord>> {
+        do_parse!(
+            i,
+            tag_no_case!("place")
+                >> space1
+                >> d: die
+                >> space1
+                >> tag_no_case!("at")
+                >> space1
+                >> c: user_coord
+                >> (GameMove::Place(d, c))
+        )
+    }
+
+    fn move_cmd(i: &str) -> IResult<&str, GameMove<UserCoord>> {
+        do_parse!(
+            i,
+            tag_no_case!("move")
+                >> space1
+                >> d: die
+                >> space1
+                >> tag_no_case!("from")
+                >> space1
+                >> from: user_coord
+                >> space1
+                >> tag_no_case!("to")
+                >> space1
+                >> to: user_coord
+                >> (GameMove::Move(d, from, to))
+        )
+    }
+
+    fn fight_cmd(i: &str) -> IResult<&str, GameMove<UserCoord>> {
+        do_parse!(
+            i,
+            tag_no_case!("fight") >> space1 >> tag_no_case!("at") >> space1 >> c: user_coord >> (GameMove::Fight(c))
+        )
+    }
+
+    fn surprise_cmd(i: &str) -> IResult<&str, GameMove<UserCoord>> {
+        do_parse!(
+            i,
+            tag_no_case!("surprise")
+                >> space1
+                >> tag_no_case!("from")
+                >> space1
+                >> from: user_coord
+                >> space1
+                >> tag_no_case!("to")
+                >> space1
+                >> to: coord
+                >> (GameMove::Surprise(from, to))
+        )
+    }
+
+    fn submit_cmd(i: &str) -> IResult<&str, GameMove<UserCoord>> {
+        value(GameMove::Submit, tag_no_case("submit"))(i)
+    }
+
+    fn game_move(i: &str) -> IResult<&str, GameMove<UserCoord>> {
+        alt((place_cmd, move_cmd, fight_cmd, surprise_cmd, submit_cmd))(i)
+    }
+
+    #[cfg(test)]
+    mod test {
+        use super::*;
+
+        macro_rules! test {
+            ($input:expr => $parsed:expr) => {
+                assert_eq!(Ok(("", $parsed)), $input);
+            };
+        }
+
+        macro_rules! test_failure {
+            ($input:expr) => {
+                assert!($input.is_err());
+            };
+        }
+
+        #[test]
+        fn test_unsigned() {
+            test!(unsigned("25") => 25);
+            test!(unsigned("0") => 0);
+
+            test_failure!(unsigned("-1"));
+            test_failure!(unsigned("250"));
+        }
+
+        #[test]
+        fn test_signed() {
+            test!(signed("25") => 25);
+            test!(signed("0") => 0);
+            test!(signed("-0") => 0);
+            test!(signed("-25") => -25);
+
+            test_failure!(signed("250"));
+        }
+
+        #[test]
+        fn test_die() {
+            test!(die("R1") => Die { color: DiceColor::Red, value: 1 });
+            test!(die("R3") => Die { color: DiceColor::Red, value: 3 });
+            test!(die("W1") => Die { color: DiceColor::White, value: 1 });
+            test!(die("b6") => Die { color: DiceColor::Black, value: 6 });
+
+            test_failure!(die("w 1"));
+            test_failure!(die("b7"));
+            test_failure!(die("r0"));
+            test_failure!(die("R0"));
+            test_failure!(die(" R1"));
+        }
+
+        #[test]
+        fn test_move() {
+            use DiceColor::*;
+            use GameMove::*;
+
+            let d = Die::new;
+            let c = Coord::new;
+            let uc = UserCoord::new;
+
+            test!(game_move("place R3 at R1C2") => Place(d(3, Red), uc(1, 2)));
+            test!(game_move("move W1 from R1C1 to R2C2") => Move(d(1, White), uc(1, 1), uc(2, 2)));
+            test!(game_move("fight at R2C1") => Fight(uc(2, 1)));
+            test!(game_move("fight at r1c3") => Fight(uc(1, 3)));
+            test!(game_move("surprise from R2C2 to <0, 1, -1>") => Surprise(uc(2, 2), c(0, 1, -1)));
+            test!(game_move("surprise FROM R1C2 TO <0,1, -1>") => Surprise(uc(1, 2), c(0, 1, -1)));
+            test!(game_move("submit") => Submit);
+            test!(game_move("SUBMIT") => Submit);
+
+            test_failure!(game_move("fight R2C1"));
+            test_failure!(game_move("fightat R2C1"));
+            test_failure!(game_move("fight at <0,0,0>"));
+        }
+    }
 }
 
 #[cfg(test)]
@@ -791,6 +972,7 @@ mod test {
         // testing moves valid in the starting position. Extend it
         // when we have `apply_move` (test placing on non-empty
         // cards).
+        use GameMove::*;
 
         let game = Game::new();
 
@@ -801,18 +983,18 @@ mod test {
         }
 
         // Positive test cases.
-        assert!(game.is_valid_move(&GameMove::Place(d(2, Red), c(0, 0))));
-        assert!(game.is_valid_move(&GameMove::Place(d(4, Red), c(0, 0))));
-        assert!(game.is_valid_move(&GameMove::Place(d(6, Red), c(0, 0))));
-        assert!(game.is_valid_move(&GameMove::Place(d(6, Red), c(1, 0))));
-        assert!(game.is_valid_move(&GameMove::Place(d(6, Red), c(2, -1))));
+        assert!(game.is_valid_move(&Place(d(2, Red), c(0, 0))));
+        assert!(game.is_valid_move(&Place(d(4, Red), c(0, 0))));
+        assert!(game.is_valid_move(&Place(d(6, Red), c(0, 0))));
+        assert!(game.is_valid_move(&Place(d(6, Red), c(1, 0))));
+        assert!(game.is_valid_move(&Place(d(6, Red), c(2, -1))));
 
         // No dice like this in Player 1's stock.
-        assert!(!game.is_valid_move(&GameMove::Place(d(5, Red), c(0, 0))));
-        assert!(!game.is_valid_move(&GameMove::Place(d(1, Black), c(0, 0))));
+        assert!(!game.is_valid_move(&Place(d(5, Red), c(0, 0))));
+        assert!(!game.is_valid_move(&Place(d(1, Black), c(0, 0))));
 
         // No card there.
-        assert!(!game.is_valid_move(&GameMove::Place(d(6, Red), c(-1, 0))));
+        assert!(!game.is_valid_move(&Place(d(6, Red), c(-1, 0))));
     }
 
     #[test]
@@ -822,6 +1004,8 @@ mod test {
         // when we have `apply_move`: test surprising twice or
         // repositioning the card after another reposition.
 
+        use GameMove::*;
+
         let game = Game::new();
 
         fn c(x: i8, y: i8) -> Coord {
@@ -829,45 +1013,45 @@ mod test {
         }
 
         // Positive test cases.
-        assert!(game.is_valid_move(&GameMove::Surprise(c(0, 0), c(1, 1))));
-        assert!(game.is_valid_move(&GameMove::Surprise(c(0, 0), c(2, 1))));
-        assert!(game.is_valid_move(&GameMove::Surprise(c(0, 0), c(2, -2))));
-        assert!(game.is_valid_move(&GameMove::Surprise(c(0, 0), c(4, -1))));
-        assert!(game.is_valid_move(&GameMove::Surprise(c(0, 0), c(3, -2))));
+        assert!(game.is_valid_move(&Surprise(c(0, 0), c(1, 1))));
+        assert!(game.is_valid_move(&Surprise(c(0, 0), c(2, 1))));
+        assert!(game.is_valid_move(&Surprise(c(0, 0), c(2, -2))));
+        assert!(game.is_valid_move(&Surprise(c(0, 0), c(4, -1))));
+        assert!(game.is_valid_move(&Surprise(c(0, 0), c(3, -2))));
 
-        assert!(game.is_valid_move(&GameMove::Surprise(c(1, 0), c(0, -1))));
-        assert!(game.is_valid_move(&GameMove::Surprise(c(1, 0), c(2, 1))));
-        assert!(game.is_valid_move(&GameMove::Surprise(c(1, 0), c(2, -2))));
-        assert!(game.is_valid_move(&GameMove::Surprise(c(1, 0), c(4, -1))));
-        assert!(game.is_valid_move(&GameMove::Surprise(c(1, 0), c(3, -2))));
+        assert!(game.is_valid_move(&Surprise(c(1, 0), c(0, -1))));
+        assert!(game.is_valid_move(&Surprise(c(1, 0), c(2, 1))));
+        assert!(game.is_valid_move(&Surprise(c(1, 0), c(2, -2))));
+        assert!(game.is_valid_move(&Surprise(c(1, 0), c(4, -1))));
+        assert!(game.is_valid_move(&Surprise(c(1, 0), c(3, -2))));
 
-        assert!(game.is_valid_move(&GameMove::Surprise(c(1, -1), c(0, 1))));
-        assert!(game.is_valid_move(&GameMove::Surprise(c(1, -1), c(1, 1))));
-        assert!(game.is_valid_move(&GameMove::Surprise(c(1, -1), c(2, 1))));
-        assert!(game.is_valid_move(&GameMove::Surprise(c(1, -1), c(4, -1))));
-        assert!(game.is_valid_move(&GameMove::Surprise(c(1, -1), c(3, -2))));
+        assert!(game.is_valid_move(&Surprise(c(1, -1), c(0, 1))));
+        assert!(game.is_valid_move(&Surprise(c(1, -1), c(1, 1))));
+        assert!(game.is_valid_move(&Surprise(c(1, -1), c(2, 1))));
+        assert!(game.is_valid_move(&Surprise(c(1, -1), c(4, -1))));
+        assert!(game.is_valid_move(&Surprise(c(1, -1), c(3, -2))));
 
-        assert!(game.is_valid_move(&GameMove::Surprise(c(2, -1), c(0, -1))));
-        assert!(game.is_valid_move(&GameMove::Surprise(c(2, -1), c(0, 1))));
-        assert!(game.is_valid_move(&GameMove::Surprise(c(2, -1), c(1, 1))));
-        assert!(game.is_valid_move(&GameMove::Surprise(c(2, -1), c(2, 1))));
-        assert!(game.is_valid_move(&GameMove::Surprise(c(2, -1), c(4, -1))));
+        assert!(game.is_valid_move(&Surprise(c(2, -1), c(0, -1))));
+        assert!(game.is_valid_move(&Surprise(c(2, -1), c(0, 1))));
+        assert!(game.is_valid_move(&Surprise(c(2, -1), c(1, 1))));
+        assert!(game.is_valid_move(&Surprise(c(2, -1), c(2, 1))));
+        assert!(game.is_valid_move(&Surprise(c(2, -1), c(4, -1))));
 
         // Can't reposition to the same spot.
-        assert!(!game.is_valid_move(&GameMove::Surprise(c(0, 0), c(0, 0))));
-        assert!(!game.is_valid_move(&GameMove::Surprise(c(1, 0), c(1, 0))));
+        assert!(!game.is_valid_move(&Surprise(c(0, 0), c(0, 0))));
+        assert!(!game.is_valid_move(&Surprise(c(1, 0), c(1, 0))));
 
         // Not enough neighbours.
-        assert!(!game.is_valid_move(&GameMove::Surprise(c(0, 0), c(0, 1))));
-        assert!(!game.is_valid_move(&GameMove::Surprise(c(0, 0), c(0, -1))));
-        assert!(!game.is_valid_move(&GameMove::Surprise(c(1, 0), c(1, 1))));
-        assert!(!game.is_valid_move(&GameMove::Surprise(c(1, 0), c(0, 1))));
-        assert!(!game.is_valid_move(&GameMove::Surprise(c(1, -1), c(0, -1))));
-        assert!(!game.is_valid_move(&GameMove::Surprise(c(1, -1), c(2, -2))));
-        assert!(!game.is_valid_move(&GameMove::Surprise(c(2, -1), c(3, -2))));
-        assert!(!game.is_valid_move(&GameMove::Surprise(c(2, -1), c(2, -2))));
+        assert!(!game.is_valid_move(&Surprise(c(0, 0), c(0, 1))));
+        assert!(!game.is_valid_move(&Surprise(c(0, 0), c(0, -1))));
+        assert!(!game.is_valid_move(&Surprise(c(1, 0), c(1, 1))));
+        assert!(!game.is_valid_move(&Surprise(c(1, 0), c(0, 1))));
+        assert!(!game.is_valid_move(&Surprise(c(1, -1), c(0, -1))));
+        assert!(!game.is_valid_move(&Surprise(c(1, -1), c(2, -2))));
+        assert!(!game.is_valid_move(&Surprise(c(2, -1), c(3, -2))));
+        assert!(!game.is_valid_move(&Surprise(c(2, -1), c(2, -2))));
 
         // Can't start from empty spot.
-        assert!(!game.is_valid_move(&GameMove::Surprise(c(3, 3), c(0, 1))));
+        assert!(!game.is_valid_move(&Surprise(c(3, 3), c(0, 1))));
     }
 }
